44a432b5ddeb1b370c4a2da82779f8e9aa26f78a
nanli9 <nanli9@asu.edu>
2023-03-09 14:34:11 -0700

    fix pause menu

--- 18C_Web_Based_Game/Assets/Scripts/SceneLoader.cs
+++ 18C_Web_Based_Game/Assets/Scripts/SceneLoader.cs
@@ -5,8 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] GameObject Pause_menu;
     public void LoadScene(string sceneName)
     {
 	    SceneManager.LoadScene(sceneName);
+    }
+    public void Pause()
+    {
+        Pause_menu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        Pause_menu.SetActive(false);
+        Time.timeScale = 1f;
     }
 }
